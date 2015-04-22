9bd8fd4a882d5e2e2536449ad1fff71a908c190e
Wideweb <alckevich@live.com>
2015-04-22 19:36:33 +0300

    CloackLib is new Library for project

---
+++ CloackLib/MechanicalWatches.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CloackLib
+{
+    public class MechanicalWatches : Cloack
+    {
+        private int engine;
+        private int TransmissionGears;
+        private int Regulator;
+    }
+}
